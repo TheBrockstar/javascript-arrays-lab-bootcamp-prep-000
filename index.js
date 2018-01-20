e22131ed3a7a3c4b077c681f79157ddaf7a8174a
Brock Rohloff <00agentb@gmail.com>
2018-01-20 21:42:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,38 @@ function destructivelyAppendKitten(name){
   kittens.push(name);
   return kittens;
 }
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop();
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift();
+  return kittens;
+}
+
+function appendKitten(name){
+  newKittens = [...kittens,name];
+  return newKittens;
+}
+
+function prependKitten(name){
+  newKittens = [name,...kittens];
+  return newKittens;
+}
+
+function removeLastKitten(){
+  newKitten = kitten.slice(0,kitten.length - 1);
+  return newKitten;
+}
+
+function removeFirstKitten(){
+  newKitten = kitten.slice(1);
+  return newKitten;
+}
